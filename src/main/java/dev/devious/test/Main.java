7dc7ba97f8e284b7951e1216dbc8b78326ad71c0
DrVonDevious <william.prometheus@gmail.com>
2022-07-08 15:28:04 -0500

    initial commit

---
+++ src/main/java/dev/devious/test/Main.java
@@ -0,0 +1,9 @@
+package dev.devious.test;
+
+import org.lwjgl.Version;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Version.getVersion());
+    }
+}
